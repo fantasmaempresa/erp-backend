d8d728e14c143deb891ce901e7bbaf39f81e1a46
Alejandro <alexrodriguezserrano1595@gmail.com>
2022-01-27 17:55:42 -0600

    :ambulance:
    agrego cors

--- public/index.php
+++ public/index.php
@@ -45,6 +45,10 @@ require __DIR__.'/../vendor/autoload.php';
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Credentials: true');
 
 $kernel = $app->make(Kernel::class);
 
