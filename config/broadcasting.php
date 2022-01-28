657ed5259ba06f6b684ab0e998e0135167af640f
Alejandro <alexrodriguezserrano1595@gmail.com>
2022-01-27 18:09:47 -0600

    :ambulance:
    agrego cors

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
 //                'useTLS' => true,
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
+                'encrypted' => false,
                 'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'http'
