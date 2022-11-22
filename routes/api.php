9a27d72595d2d96f21ffec098e5350cf7fb9d366
Alejandro <alexrodriguezserrano1595@gmail.com>
2022-11-22 12:45:35 -0600

    :fix:
    change api

--- routes/api.php
+++ routes/api.php
@@ -50,7 +50,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth:api', 'permission']], function () {
     Route::resource('roles', RoleController::class, ['except' => ['create', 'edit']]);
-    Route::get('roles/modules', [RoleActionController::class, 'getModules']);
+    Route::get('roles/modules/get', [RoleActionController::class, 'getModules']);
     Route::get('roles/modules/construct/{role}', [RoleActionController::class, 'constructMenu']);
 
     Route::resource('users', UserController::class, ['except' => ['create', 'edit']]);
