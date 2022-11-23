a29da3d33edb21934a18c25b1271c98b4cc98d25
Alejandro <alexrodriguezserrano1595@gmail.com>
2022-11-23 17:25:02 -0600

    :fix:

--- app/Http/Controllers/Role/RoleActionController.php
+++ app/Http/Controllers/Role/RoleActionController.php
@@ -162,6 +162,7 @@ class RoleActionController extends ApiController
                 $menus['submenus'][] = $menu;
             }
         } else {
+            dd($user->role->config);
             $config = json_decode($user->role->config);
             foreach ($config['modules'] as $module) {
                 foreach (self::$startMenu as $menu) {
