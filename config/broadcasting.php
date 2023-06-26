897c3143cf03b55a26ffb5f0b83e615d5e303808
Alejandro <alexrodriguezserrano1595@gmail.com>
2023-06-26 14:14:02 -0600

    ssl websockets

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,13 +36,17 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'useTLS' => true,
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
+//                'useTLS' => true,
+//                'encrypted' => true,
 //                'host' => '127.0.0.1',
                 'host' => 'notaria4puebla.com.mx',
                 'port' => 6003,
-                'scheme' => 'https'
+                'scheme' => 'https',
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ]
             ],
         ],
 
