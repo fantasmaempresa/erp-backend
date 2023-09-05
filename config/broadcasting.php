6bf822a9b88a78b20a63da2355ed62c80f00a3de
Alejandro <alexrodriguezserrano1595@gmail.com>
2023-09-04 18:48:54 -0600

    reports

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,10 +39,10 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
 //                'useTLS' => true,
 //                'encrypted' => true,
-                'host' => '127.0.0.1',
-//                'host' => 'notaria4puebla.com.mx',
+//                'host' => '127.0.0.1',
+                'host' => 'notaria4puebla.com.mx',
                 'port' => 6003,
-                'scheme' => 'http',
+                'scheme' => 'https',
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
