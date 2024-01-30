348e838ae990a68cb4f3a19b4147945985fe5ac5
Erick Dylan Navarrete de la Vega <ericknavarrete55@gmail.com>
2024-01-29 21:36:45 -0600

    :sparkles: Modifco el controlador de Usuarios

--- app/Models/DisposalRealEstate.php
+++ app/Models/DisposalRealEstate.php
@@ -8,6 +8,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /**
  * @version
@@ -64,27 +66,57 @@ class DisposalRealEstate extends Model
     ];
 
     /**
-     * @return string[]
+     * @return BelongsTo
      */
-    public static function rules(): array
+    public function nationalConsumerPriceIndexDisposal(): BelongsTo
     {
-        return [
-            'type_disposal_operation_id' => 'required|int',
-            'disposal_value' => 'required|numeric',
-            'disposal_date' => 'required|date',
-            'acquisition_value' => 'required|numeric',
-            'acquisition_date' => 'required|date',
-            'real_estate_appraisal' => 'required|numeric',
-            'fiscal_appraisal' => 'required|numeric',
-            'land_proportion' => 'required|int',
-            'construction_proportion' => 'int',
-            'depreciation_rate' => 'required|int',
-            'improvements' => 'numeric',
-            'appraisal' => 'numeric',
-            'commissions' => 'numeric',
-            'isabi' => 'numeric',
-            'rate' => 'required|int',
-        ];
+        return $this->belongsTo(NationalConsumerPriceIndex::class, 'ncpi_disposal_id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function nationalConsumerPriceIndexAcquisition(): BelongsTo
+    {
+        return $this->belongsTo(NationalConsumerPriceIndex::class, 'ncpi_acquisition_id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function alienating(): BelongsTo
+    {
+        return $this->belongsTo(Grantor::class, 'alienating_id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function typeDisposalOperation(): BelongsTo
+    {
+        return $this->belongsTo(TypeDisposalOperation::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function rate(): BelongsTo
+    {
+        return $this->belongsTo(Rate::class);
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function acquirers(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Grantor::class,
+            'acquirer_disposal_real_estates',
+            'disposal_real_estate_id',
+            'acquirer_id'
+        )
+            ->withPivot(['proportion', 'operation_mount_value', 'tax_base', 'rate', 'isr_acquisition']);
     }
 
     /**
@@ -110,4 +142,34 @@ class DisposalRealEstate extends Model
             return $npci;
         }
     }
+
+    /**
+     * @return string[]
+     */
+    public static function rules(): array
+    {
+        return [
+            'alienating_id' => 'required|int',
+            'type_disposal_operation_id' => 'required|int',
+            'disposal_value' => 'required|numeric',
+            'disposal_date' => 'required|date',
+            'acquisition_value' => 'required|numeric',
+            'acquisition_date' => 'required|date',
+            'real_estate_appraisal' => 'required|numeric',
+            'fiscal_appraisal' => 'required|numeric',
+            'land_proportion' => 'required|int',
+            'construction_proportion' => 'int',
+            'depreciation_rate' => 'required|int',
+            'improvements' => 'numeric',
+            'appraisal' => 'numeric',
+            'commissions' => 'numeric',
+            'isabi' => 'numeric',
+            'rate' => 'required|int',
+            'acquirers' => 'required|array',
+            'acquirers.*.id' => 'required|int',
+            'acquirers.*.proportion' => 'required|int',
+            'acquirers.*.rate' => 'required|int',
+        ];
+    }
+
 }
