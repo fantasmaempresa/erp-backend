8033303b08f1186be88f98702963c33b8b944a6e
Erick Dylan Navarrete de la Vega <ericknavarrete55@gmail.com>
2024-03-20 22:07:02 -0600

    :sparkles: Agrego campo Type a la tabla

--- app/Models/ProcessingIncome.php
+++ app/Models/ProcessingIncome.php
@@ -6,11 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProcessingIncome extends Model
 {
+    const DOCUMENT_REGISTER = 1;
+    const ENTRY_TICKET = 2;
+    const DOCUMENT_RETURN = 3;
+
     protected $fillable = [
       'id',
       'name',
       'date_income',
       'config',
+      'type',
       'procedure_id',
       'operation_id',
       'staff_id',
@@ -55,9 +60,10 @@ class ProcessingIncome extends Model
             'name' => 'required|string',
             'date_income' => 'required|date',
             'config' => 'nullable|array',
+            'type' => 'required|in:'.self::DOCUMENT_REGISTER.','.self::ENTRY_TICKET.','.self::DOCUMENT_RETURN,
             'procedure_id' => 'required|exists:procedures,id',
             'operation_id' => 'required|exists:operations,id',
-            'staff_id' => 'required|exists:staffs,id',
+            'staff_id' => 'required|exists:staff,id',
             'place_id' => 'required|exists:places,id',
         ];
     }
