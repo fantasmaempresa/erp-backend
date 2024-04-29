7f65a1ac74e5d4561306d2d332046c2ab80c87b6
Arniquin <arnold.richards759@gmail.com>
2024-04-28 20:34:13 -0600

    :construction: Creacion de las migraciones

---
+++ database/migrations/2024_04_23_003748_create_movement_trackings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovementTrackingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movement_trackings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('articel_id')->constrained();
+            $table->foreignId('warehouse_id')->constrained();
+            $table->integer('amount');
+            $table->string('reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movement_trackings');
+    }
+}
