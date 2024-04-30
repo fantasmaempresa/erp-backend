10fd7b334554ec1a366bf52a88e8dafc3c1aed91
Erick Dylan Navarrete de la Vega <ericknavarrete55@gmail.com>
2024-04-29 21:15:43 -0600

    :sparkles: Agrego expiración al token

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,9 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        Passport::tokensExpireIn(now()->addDays(15));
+        Passport::tokensExpireIn(now()->addDays(1));
         Passport::refreshTokensExpireIn(now()->addDays(30));
-        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
 //        if (!$this->app->routesAreCached()) {
 //            Passport::routes();
 //        }
