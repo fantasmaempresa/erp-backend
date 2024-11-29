ed0bcfba1b3e47e92f82d06213def3d524fd46da
Alejandro Rodríguez Serrano <36872513+AlexMatix@users.noreply.github.com>
2024-11-29 17:06:26 -0600

    websocket

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 // 'host' => '127.0.0.1',
+                'useTLS' => false,
                 'host' => 'back.notaria4puebla.com.mx',
                 'port' => 6003,
                 'scheme' => 'https',
