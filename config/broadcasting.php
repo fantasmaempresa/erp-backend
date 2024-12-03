6dbf858821206d14e6256a4246533589d5fc2387
Alejandro Rodríguez Serrano <36872513+AlexMatix@users.noreply.github.com>
2024-12-03 12:00:45 -0600

    :bugs: fix update package react

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,8 +37,8 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                // 'host' => '127.0.0.1',
-                'useTLS' => false,
+                // 'host' => 'localhost',
+                'useTLS' => true,
                 'host' => 'back.notaria4puebla.com.mx',
                 'port' => 6003,
                 'scheme' => 'https',
--- config/websockets.php
+++ config/websockets.php
@@ -116,15 +116,15 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        // 'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
-        'local_cert' => "/etc/letsencrypt/live/back.notaria4puebla.com.mx/fullchain.pem",
+        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
+        // 'local_cert' => "/etc/letsencrypt/live/back.notaria4puebla.com.mx/fullchain.pem",
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        // 'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
-        'local_pk' => "/etc/letsencrypt/live/back.notaria4puebla.com.mx/privkey.pem",
+        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
+        // 'local_pk' => "/etc/letsencrypt/live/back.notaria4puebla.com.mx/privkey.pem",
 
         /*
          * Passphrase for your local_cert file.
